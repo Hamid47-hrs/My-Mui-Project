33f7bae53a9d2a97de4542f832b35b1ec89b40af
Hamid.hrs <hamid47.hrs@gmail.com>
2021-12-17 15:59:12 +0330

    create necessary web app components

---
+++ src/components/layouts/MainFeed.jsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+
+
+export default function Main() {
+  return (
+    <React.Fragment>
+
+    </React.Fragment>
+  );
+}
