2b8d52ea18fcde031b88ef438627235ee031a3f8
Hamid.hrs <hamid47.hrs@gmail.com>
2021-12-18 11:09:09 +0330

    Change custom palette and add some colors

--- src/components/styles/theme/MainTheme.js
+++ src/components/styles/theme/MainTheme.js
@@ -9,17 +9,35 @@ import {
   yellow,
 } from "@material-ui/core/colors";
 const MainTheme = createTheme({
-  palette: {
-    primary: {
-      main: blue[600],
-      subsidiary: blue[700],
-      default: blue[300],
-    },
-    secondary: {
-      main: red[500],
-      subsidiary: red[700],
-      default: red[300],
-    },
+  blueStyle: {
+    main: blue[600],
+    secondary: blue[700],
+    default: blue[300],
+  },
+  redStyle: {
+    main: red[500],
+    secondary: red[700],
+    default: red[300],
+  },
+  greenStyle: {
+    main: green[500],
+    secondary: green[700],
+    default: green[300],
+  },
+  yellowStyle: {
+    main: yellow[500],
+    secondary: yellow[700],
+    default: yellow[300],
+  },
+  common: {
+    main: common["white"],
+    secondary: common["black"],
+  },
+  text: {
+    white: common["white"],
+    black: common["black"],
+    grey: "#555",
+    lightgrey: "#f8f8f8",
   },
 });
 
