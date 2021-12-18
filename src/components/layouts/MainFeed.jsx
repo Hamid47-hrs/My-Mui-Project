2635e75c53855c45fadb51b9806db3130dd4d449
Hamid.hrs <hamid47.hrs@gmail.com>
2021-12-18 23:39:57 +0330

    Add post components to main feed component

--- src/components/layouts/MainFeed.jsx
+++ src/components/layouts/MainFeed.jsx
@@ -1,11 +1,16 @@
 import * as React from "react";
 import { Container } from "@material-ui/core";
+import PostFeed from "./PostFeed";
 
 
 export default function MainFeed() {
   return (
     <Container>
-      The Main Content Goes Here.
+      <PostFeed />
+      <PostFeed />
+      <PostFeed />
+      <PostFeed />
+      <PostFeed />
     </Container>
   );
 }
